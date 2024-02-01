3db72a78087975d83d1076243d2e34db0557b199
Mudassir Shoukat <mudassirshoukat7577@gmail.com>
2024-02-01 23:00:25 +0500

    Variables

---
+++ Basics/Variables.js
@@ -0,0 +1,8 @@
+var txt = "Hello World";
+var myNum = 6;
+var check = false;
+// txt=7.8  //Not Allowed in TypeScript. Thats TS Beauty
+console.log(txt);
+console.log(myNum);
+console.log(check);
+ console.log(check+1); //allowed in ts
---
+++ Basics/Variables.ts
@@ -0,0 +1,9 @@
+let txt:string="Hello World";
+let myNum:number=6;
+let check:boolean= false;
+
+// txt=7.8  //Not Allowed in TypeScript. Thats TS Beauty
+console.log(txt);
+console.log(myNum);
+console.log(check);
+// console.log(check+1); //Not Allowed In Ts But Allowed in Js
